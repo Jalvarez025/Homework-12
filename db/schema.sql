cb5b30408d9c04c3164bac615189114b08898e27
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2021-10-27 19:32:47 -0500

    Server not done

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+-- Drops the sample_db if it exists currently --
+DROP DATABASE IF EXISTS company_db;
+-- Creates the sample_db database --
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INT,
+  -- Makes a string column called "name" which cannot contain null --
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
